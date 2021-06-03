4eecb7811f0bc5fc08167695e9dfb0fbf08e1f79
Nhu Duc <aduckuba41@gmail.com>
2021-06-03 15:16:32 +0700

    nop bai

---
+++ SQL/employee.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE employee(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    fullname varchar(255) not null,
+    dob varchar(255) not null,
+    address varchar(255) not null,
+    position varchar(255) not null,
+    department varchar(255) not null
+    );
+    INSERT INTO employee (fullname, dob, address, position, department) VALUES ('Vu Huy','20/02/2001','Ha Noi', 'Giam Doc', 'Tai Chinh');
+    
+    
+    
+    
+    
